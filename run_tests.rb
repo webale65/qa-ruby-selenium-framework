aa4c5b2aea0fe0248391feddafc429c81b151960
Alberto Alejandro <108832597+webale65@users.noreply.github.com>
2026-06-19 23:28:17 -0300

    Fix runner and headless browser setup

--- run_tests.rb
+++ run_tests.rb
@@ -1,3 +1,3 @@
 Dir[File.join(__dir__, "selenium_tests", "*_test.rb")].sort.each do |test_file|
-  require test_file
+  load test_file
 end
--- utils/test_base.rb
+++ utils/test_base.rb
@@ -1,20 +1,52 @@
-def setup
-  options = Selenium::WebDriver::Chrome::Options.new
+require 'minitest/autorun'
+require 'selenium-webdriver'
+require 'fileutils'
+require_relative '../config/settings'
 
-  options.add_argument("--headless=new")
-  options.add_argument("--no-sandbox")
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1920,1080")
-  options.add_argument("--log-level=3")
-  options.add_argument("--disable-notifications")
+class TestBase < Minitest::Test
 
-  options.add_preference("credentials_enable_service", false)
-  options.add_preference("profile.password_manager_enabled", false)
+  def setup
+    options = Selenium::WebDriver::Chrome::Options.new
 
-  @driver = Selenium::WebDriver.for(
-    SETTINGS[:browser],
-    options: options
-  )
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--log-level=3")
+    options.add_argument("--disable-notifications")
 
-  @driver.navigate.to SETTINGS[:base_url]
+    options.add_preference("credentials_enable_service", false)
+    options.add_preference("profile.password_manager_enabled", false)
+
+    @driver = Selenium::WebDriver.for(
+      SETTINGS[:browser],
+      options: options
+    )
+
+    @driver.navigate.to SETTINGS[:base_url]
+  end
+
+  def teardown
+    if passed?
+      puts "Test finalizado correctamente."
+    else
+      tomar_captura if SETTINGS[:screenshots_enabled]
+    end
+
+    @driver.quit if @driver
+  rescue
+    puts "El navegador ya estaba cerrado."
+  end
+
+  def tomar_captura
+    FileUtils.mkdir_p("screenshots")
+
+    timestamp = Time.now.strftime("%Y%m%d_%H%M%S")
+    file_name = "screenshots/error_#{timestamp}.png"
+
+    @driver.save_screenshot(file_name)
+
+    puts "Captura guardada en: #{file_name}"
+  end
+
 end
